8a4adf73c8f5a7dfb900bf74a0ac1c5e757a4e41
Eduardo <eduardojunior010757@gmail.com>
2024-07-16 20:50:32 -0300

    created task model

--- companies/models.py
+++ companies/models.py
@@ -8,3 +8,21 @@ class Enterprise(models.Model):
 class Employee(models.Model):
     user = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
     enterprise = models.ForeignKey(Enterprise, on_delete=models.CASCADE)
+
+
+class TaskStatus(models.Model):
+    name = models.CharField(max_length=155)
+    code_name = models.CharField(max_length=100)
+    class Meta:
+        db_table = "companies_task_status"
+
+
+class Task(models.Model):
+    title = models.TextField()
+    description = models.TextField(null=True)
+    due_data = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(null=True)
+    updated_at = models.DateTimeField(null=True)
+    status = models.ForeignKey(TaskStatus, on_delete=models.CASCADE)
+    enterprise = models.ForeignKey(Enterprise, on_delete=models.CASCADE)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
